aa7517a8db18cbaac9f23e54b2d2c6aa93fbe0cb
Lukas Silveira <lukassilveira12@hotmail.com>
2023-03-19 23:47:33 -0300

    creating navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import NavBarComponent from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <NavBarComponent>
+        
+      </NavBarComponent>
       <div className="content">
         <h1>App Component</h1>
       </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBarComponent = () => {
+    return ( 
+        <nav>
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </nav>
+    );
+}
+ 
+export default NavBarComponent;
