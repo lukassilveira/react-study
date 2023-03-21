c17752c033c0cb706888841275957d1a2cbd1ef2
Lukas Silveira <lukassilveira12@hotmail.com>
2023-03-20 23:23:31 -0300

    creating home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import NavBarComponent from './components/NavBar';
+import "./App.css";
+import HomeComponent from "./components/Home";
+import NavBarComponent from "./components/NavBar";
 
 function App() {
-  return (
-    <div className='App'>
-      <NavBarComponent>
-        
-      </NavBarComponent>
-      <div className="content">
-        <h1>App Component</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBarComponent>/</NavBarComponent>
+			<div className="content">
+				<HomeComponent></HomeComponent>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const HomeComponent = () => {
+    return ( 
+        <div className="home">
+            <p>
+                Home
+            </p>
+        </div>
+     );
+}
+ 
+export default HomeComponent;
