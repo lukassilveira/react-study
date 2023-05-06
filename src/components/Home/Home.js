bd606f8c54811171c71ff282a805344f17e2d8b4
Lukas Silveira <lukassilveira12@hotmail.com>
2023-05-06 15:47:19 -0300

    using state hook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,22 @@
-import "./Home.css"
+import "./Home.css";
+import { useState } from "react";
 
 const HomeComponent = () => {
-    return ( 
-        <div className="home">
-            <p className="home-title">
-                Home
-            </p>
-        </div>
-     );
-}
- 
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("Yoshi");
+        setAge(10);
+    };
+	return (
+		<div className="home">
+			<p className="home-title">Home</p>
+			<p>{name} is {age} years old.</p>
+		
+			<button onClick={handleClick}>Button</button>
+		</div>
+	);
+};
+
 export default HomeComponent;
