56710fa0f235cbe8b6c68438685a5867473d8dff
Lukas Silveira <lukassilveira12@hotmail.com>
2023-05-13 02:03:31 -0300

    addying style and using array for posts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,19 +2,28 @@ import "./Home.css";
 import { useState } from "react";
 
 const HomeComponent = () => {
-    const [name, setName] = useState("Mario");
-    const [age, setAge] = useState(25);
+	const [posts, setPosts] = useState([
+		{ title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+		{ title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+		{
+			title: "Web dev top tips",
+			body: "lorem ipsum...",
+			author: "mario",
+			id: 3,
+		},
+	]);
 
-    const handleClick = () => {
-        setName("Yoshi");
-        setAge(10);
-    };
 	return (
-		<div className="home">
-			<p className="home-title">Home</p>
-			<p>{name} is {age} years old.</p>
-		
-			<button onClick={handleClick}>Button</button>
+		<div className="home w-50 mx-auto">
+			<p className="home-title py-3 text-center">Home</p>
+			<div className="home-content d-flex flex-column">
+				{posts.map((post) => (
+					<div className="post d-flex flex-column align-items-start w-100 mb-5 p-3" key={post.id}>
+						<span className="post-title">{post.title}</span>
+						<span className="post-body">{post.title}</span>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
