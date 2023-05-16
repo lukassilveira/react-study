14da8519836ae68373689b99d36aee1f6d689606
Lukas Silveira <lukassilveira12@hotmail.com>
2023-05-15 23:47:34 -0300

    adding delete button

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -3,8 +3,9 @@ import "./PostCard.css";
 const PostCard = ({ title, author, body }) => {
 	return (
 		<div className="post d-flex flex-column align-items-start w-100 mb-5 p-3">
-			<div className="post-header">
+			<div className="post-header d-flex w-100 align-items-center justify-content-between">
 				<span className="post-title">{title}</span>
+				<button className="btn btn-light delete-button">Delete</button>
 			</div>
 			<div className="post-body">
 				<span className="post-title">{body}</span>
